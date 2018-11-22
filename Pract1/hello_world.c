702a0f27a689ad1203be29c818568055f58542d6
ahidalgob <ahidalgo94@gmail.com>
2018-11-22 17:25:53 -0400

    renaming

--- Pract1/hello_world.c
+++ Pract1/hello_world.c
@@ -1,27 +1,14 @@
-#include <mpi.h>
+#include "mpi.h"
 #include <stdio.h>
 
-int main(int argc, char** argv) {
-    // Initialize the MPI environment
-    MPI_Init(NULL, NULL);
-
-    // Get the number of processes
-    int world_size;
-    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
-
-    // Get the rank of the process
-    int world_rank;
-    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
-
-    // Get the name of the processor
-    char processor_name[MPI_MAX_PROCESSOR_NAME];
-    int name_len;
-    MPI_Get_processor_name(processor_name, &name_len);
-
-    // Print off a hello world message
-    printf("Hello world from processor %s, rank %d out of %d processors\n",
-           processor_name, world_rank, world_size);
-
-    // Finalize the MPI environment.
-    MPI_Finalize();
+int main(int argc, char *argv[]) {
+	int me, numProcess;
+	MPI_Init(&argc, &argv);
+	MPI_Comm_rank(MPI_COMM_WORLD, &me);
+	MPI_Comm_size(MPI_COMM_WORLD, &numProcess);
+	if (me == 0)
+		printf ("Se han creado %d procesos\n", numProcess);
+	printf ("Soy el proceso %d\n", me);
+	MPI_Finalize();
+	return 0;
 }
